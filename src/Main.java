492d4c742264f59a035da0cd2771b87b13881193
James <tigre93120@hotmail.com>
2021-11-25 08:45:44 +0100

    test conflit

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,11 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.TreeMap;	
+import java.util.TreeMap;
 import java.util.TreeSet;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import Set.Element;
