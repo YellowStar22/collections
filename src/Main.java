d67a38c546360018f92f6f7e1e64012f5759be6b
James <tigre93120@hotmail.com>
2021-11-25 10:22:07 +0100

    Test staging area

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,11 @@ public class Main {
 	
 
 	public static void main(String[] args) {
+		
+		String s = "Hello to Java !";
+		for (String string : s.split(" ")) {
+			System.out.println(string);
+		}
 	}
 	
 }
